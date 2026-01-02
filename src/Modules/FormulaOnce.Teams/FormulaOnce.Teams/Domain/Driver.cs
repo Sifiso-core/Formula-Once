6e93e96554e62255701a267bedf10627158ce9dd
Sifiso <Sifisodlamini123p@gmail.com>
2026-01-02 13:08:55 +0200

    Initial Folder Structure with Host api and teams module definition

---
+++ src/Modules/FormulaOnce.Teams/FormulaOnce.Teams/Domain/Driver.cs
@@ -0,0 +1,27 @@
+﻿namespace FormulaOnce.Teams.Domain;
+
+public class Driver
+{
+    private Driver()
+    {
+        //For EF Core
+    }
+
+    public Guid Id { get; private set; }
+    public string Name { get; private set; } = string.Empty;
+    public string Surname { get; private set; } = string.Empty;
+    public string FullName => $"{Name} {Surname}";
+
+    public string Abbreviation { get; private set; } = string.Empty;
+    public int PermanentNumber { get; private set; }
+    public int? RacingNumber { get; private set; }
+
+    public string Nationality { get; private set; } = string.Empty;
+    public string? PlaceOfBirth { get; private set; }
+    public DateTime DateOfBirth { get; init; }
+    public Guid ConstructorId { get; private set; }
+    public int Age { get; private set; }
+    public Stats CareerStats { get; private set; } = null!;
+
+    
+}
